e2b691fd41b9b96809c0460b27d490141500fd0b
David Roško <rosko.dav@gmail.com>
2016-11-02 09:02:25 +0100

    Add Tests

--- KeboolaChatbot/BotToUserLogger.cs
+++ KeboolaChatbot/BotToUserLogger.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
-using System.Web;
 using Microsoft.Bot.Builder.Dialogs.Internals;
 using Microsoft.Bot.Builder.Internals.Fibers;
 using Microsoft.Bot.Connector;
@@ -13,8 +8,8 @@ namespace KeboolaChatbot
 {
     public class BotToUserLogger : IBotToUser
     {
-        private readonly IMessageActivity _toBot;
         private readonly IConnectorClient _client;
+        private readonly IMessageActivity _toBot;
 
         public BotToUserLogger(IMessageActivity toBot, IConnectorClient client)
         {
@@ -24,29 +19,33 @@ namespace KeboolaChatbot
 
         public IMessageActivity MakeMessage()
         {
-            var toBotActivity = (Activity)_toBot;
+            var toBotActivity = (Activity) _toBot;
             return toBotActivity.CreateReply();
         }
 
-        public async Task PostAsync(IMessageActivity message, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task PostAsync(IMessageActivity message,
+            CancellationToken cancellationToken = default(CancellationToken))
         {
-            await _client.Conversations.ReplyToActivityAsync((Activity)message, cancellationToken);
+            await _client.Conversations.ReplyToActivityAsync((Activity) message, cancellationToken);
         }
     }
 
     public class BotToUserDatabaseWriter : IBotToUser
     {
         private readonly IBotToUser _inner;
+
         public BotToUserDatabaseWriter(IBotToUser inner)
         {
             SetField.NotNull(out _inner, nameof(inner), inner);
         }
+
         public IMessageActivity MakeMessage()
         {
             return _inner.MakeMessage();
         }
 
-        public async Task PostAsync(IMessageActivity message, CancellationToken cancellationToken = default(CancellationToken))
+        public async Task PostAsync(IMessageActivity message,
+            CancellationToken cancellationToken = default(CancellationToken))
         {
             //TODO log outgoing messages
 
